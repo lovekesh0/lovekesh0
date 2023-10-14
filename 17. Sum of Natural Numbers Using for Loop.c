af477029f32121937d21a742ffbf59bd2e8546b7
lovekesh0 <147271252+lovekesh0@users.noreply.github.com>
2023-10-15 01:58:40 +0530

    Create 17. Sum of Natural Numbers Using for Loop.c

---
+++ 17. Sum of Natural Numbers Using for Loop.c
@@ -0,0 +1,17 @@
+// program no. 17 : Sum of Natural Numbers Using for Loop
+
+#include <stdio.h>
+int main()
+{
+    int n, i, sum = 0;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; ++i) {
+        sum += i;
+    }
+
+    printf("Sum = %d", sum);
+    return 0;
+}
